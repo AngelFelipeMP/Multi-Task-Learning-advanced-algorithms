dfd0459fd6f0180bd1db66b7119f07f125b6531c
AngelFelipeMP <angel.magnossao@gmail.com>
2022-08-23 14:55:52 +0200

    "datalodare validation df3"

--- sampler_val.py
+++ sampler_val.py
@@ -1,5 +1,6 @@
 import math
 import torch
+from torch.utils.data.dataset import ConcatDataset
 from torch.utils.data.sampler import RandomSampler
 
 
@@ -11,42 +12,62 @@ class BatchSchedulerSampler(torch.utils.data.sampler.Sampler):
         self.dataset = dataset
         self.batch_size = batch_size
         self.number_of_datasets = len(dataset.datasets)
-        # self.smallest_dataset_size = min([len(cur_dataset.samples) for cur_dataset in dataset.datasets])
-        self.largest_dataset_size = max([len(cur_dataset.samples) for cur_dataset in dataset.datasets])
+        self.smallest_dataset_size = min([len(cur_dataset.samples) for cur_dataset in dataset.datasets])
+        self.interactions = math.ceil(self.smallest_dataset_size / self.batch_size)
 
     def __len__(self):
-        return self.batch_size * math.ceil(self.largest_dataset_size / self.batch_size) * len(self.dataset.datasets)
+        return self.batch_size * math.ceil(self.smallest_dataset_size / self.batch_size) * len(self.dataset.datasets)
 
     def __iter__(self):
         samplers_list = []
         sampler_iterators = []
         datasets_size = []
+        batch_size = []
+        start_interactions = []
         
         for dataset_idx in range(self.number_of_datasets):
+            
             cur_dataset = self.dataset.datasets[dataset_idx]
-            datasets_size.append(len(cur_dataset))
+            datasets_size.append(len(cur_dataset ))
             sampler = RandomSampler(cur_dataset)
             samplers_list.append(sampler)
             cur_sampler_iterator = sampler.__iter__()
             sampler_iterators.append(cur_sampler_iterator)
+            
+            batch = datasets_size[dataset_idx]//self.interactions
+            print('#'*50)
+            print(batch)
+            batch_size.append(batch)
+            sample = batch*self.interactions
+            print(sample)
+            remainder = datasets_size[dataset_idx] - sample
+            print(remainder)
+            start_interactions.append(self.interactions - remainder)
+            print(self.interactions - remainder)
 
         push_index_val = [0] + self.dataset.cumulative_sizes[:-1]
-        step = self.batch_size * self.number_of_datasets
-        # interactions = math.ceil(self.smallest_dataset_size / self.batch_size)
-        interactions = math.ceil(self.largest_dataset_size / self.batch_size)
-        
+        # step = self.batch_size * self.number_of_datasets # I don't need it 
         # epoch_samples = self.smallest_dataset_size * self.number_of_datasets
-        epoch_samples = self.largest_dataset_size * self.number_of_datasets
-
         final_samples_list = []  # this is a list of indexes from the combined dataset
-        for _ in range(0, epoch_samples, step):
+        
+        # for j, _ in enumerate(range(0, epoch_samples, step)):  # I don't need it  # interations
+        print('@'*50)
+        print(self.interactions)
+        
+        ########### !!!!!!!!!!!!!!! ANALISE the following code !!!!!!!!!!!!!!!!!!!! ##############
+        ########### !!!!!!!!!!!!!!! ANALISE inside loop !!!!!!!!!!!!!!!!!!!! ##############
+        for j in range(1, self.interactions):
+            print('*'*50)
+            print(j)
             
             for i in range(self.number_of_datasets):
+                print('&'*50)
+                print(start_interactions[i])
+                
+                samples_to_grab = batch_size[i] if j < start_interactions[i] else batch_size[i] + 1
                 
                 cur_batch_sampler = sampler_iterators[i]
                 cur_samples = []
-                samples_to_grab =  math.ceil(datasets_size[i] / interactions)
-
                 for _ in range(samples_to_grab):
                     try:
                         cur_sample_org = cur_batch_sampler.__next__()
@@ -59,4 +80,49 @@ class BatchSchedulerSampler(torch.utils.data.sampler.Sampler):
         return iter(final_samples_list)
     
     
-    ### !!!! Retrive diferente number of samples for each dataset but have the same number of interactions ########
+    
+class MyFirstDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        # dummy dataset
+        self.samples = torch.cat((-torch.ones(5), torch.ones(5)))
+
+    def __getitem__(self, index):
+        # change this to your samples fetching logic
+        return self.samples[index]
+
+    def __len__(self):
+        # change this to return number of samples in your dataset
+        return self.samples.shape[0]
+
+
+class MySecondDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        # dummy dataset
+        self.samples = torch.cat((torch.ones(10) * 5, torch.ones(5) * -5))
+
+    def __getitem__(self, index):
+        # change this to your samples fetching logic
+        return self.samples[index]
+
+    def __len__(self):
+        # change this to return number of samples in your dataset
+        return self.samples.shape[0]   
+
+if __name__ == "__main__":
+    first_dataset = MyFirstDataset()
+    second_dataset = MySecondDataset()
+    concat_dataset = ConcatDataset([first_dataset, second_dataset])
+    
+    
+
+    batch_size = 3
+
+    # dataloader with BatchSchedulerSampler
+    dataloader = torch.utils.data.DataLoader(dataset=concat_dataset,
+                                            sampler=BatchSchedulerSampler(dataset=concat_dataset,
+                                                                        batch_size=batch_size),
+                                            batch_size=batch_size,
+                                            shuffle=False)
+
+    for inputs in dataloader:
+        print(inputs)
